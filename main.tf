7c55c7021960367ef9f5499dcca73a79241db761
Cristian Lopez <62627441+Cristixndr3s@users.noreply.github.com>
2024-05-25 11:16:56 -0500

    Create main.tf
    falta <path_to_your_service_account_key>.json"
    y project ID

---
+++ main.tf
@@ -0,0 +1,53 @@
+provider "google" {
+  credentials = file("<path_to_your_service_account_key>.json")
+  project     = var.project
+  region      = var.region
+}
+
+variable "project" {
+  default = "<YOUR_PROJECT_ID>"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+
+    access_config {
+      // Include this section to give the VM an external IP address
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y python3 python3-pip
+    echo 'print("Hello, World!")' > app.py
+    nohup python3 app.py &
+  EOT
+}
+
+output "instance_ip" {
+  value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
+}
